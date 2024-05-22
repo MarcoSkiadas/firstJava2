bd7941dc0b017141a8df0695128de7f7e07c3c47
JulianeSchrittenlacher <julianeschrittenlacher@gmail.com>
2024-05-22 16:14:53 +0200

    add hello Ursula

--- src/main/java/org/example/HalloMarco.java
+++ src/main/java/org/example/HalloMarco.java
@@ -3,5 +3,6 @@ package org.example;
 public class HalloMarco {
     public static void main(String[] args) {
         System.out.println("Hallo Marco");
+        System.out.println("Hallo Ursula");
     }
 }
